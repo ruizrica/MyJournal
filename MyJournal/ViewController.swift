21f145568e805dad015577fda31cac0bcf8ac48d
Ricardo Ruiz <ricardo@kenetic-labs.com>
2017-06-07 18:24:21 -0700

    Initial Commit

---
+++ MyJournal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyJournal
+//
+//  Created by Ricardo Ruiz on 6/7/17.
+//  Copyright © 2017 Ricardo Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
